bce63287cd6cf5f934eeebe2e205e7eed30d9153
Alexey Mokharev <lmokhareva@gmail.com>
2021-05-21 00:30:42 +0300

    Fixed some bags

--- north_western_method.py
+++ north_western_method.py
@@ -1,3 +1,6 @@
+import random
+
+
 def make_basis(transport_table_price, demand_colomn, production_line):
     transportation_burden = [[0] * len(transport_table_price[0]) for i in
                              range(len(transport_table_price))]  # создаем таблицу для хранения объема перевозки
@@ -6,11 +9,11 @@ def make_basis(transport_table_price, demand_colomn, production_line):
     residue_colomn = demand_colomn  # храним остаток спроса
 
     for i in range(len(transportation_burden)):
-        for j in range(len(transport_table_price[0])):
+        for j in range(len(transportation_burden[0])):
             if residue_colomn[i] == 0 or residue_line[j] == 0:  # проверка на нуль остаток производства и спроса
                 continue
 
-            if production_line[j] <= demand_colomn[i]:  # если спроси больше либо равен предложению
+            if production_line[j] <= demand_colomn[i]:  # если спрос больше, либо равен предложению
                 transportation_burden[i][j] = production_line[j]
                 residue_colomn[i] = demand_colomn[i] - production_line[j]
                 residue_line[j] = 0
@@ -20,3 +23,34 @@ def make_basis(transport_table_price, demand_colomn, production_line):
                 residue_colomn[i] = 0
 
     print(transportation_burden)
+    print("Total price is: " + str(make_price(transport_table_price, transportation_burden)))
+
+
+def make_price(transport_table_price, transportation_burden):
+    final_price = 0
+
+    for i in range(len(transportation_burden)):
+        for j in range(len(transportation_burden[0])):
+            final_price += transport_table_price[i][j] * transportation_burden[i][j]
+
+    return final_price
+
+
+def find_max_price(transport_table_price):
+    max = 0
+    max_colomn = 0
+    max_line = 0
+
+    for i in range(len(transport_table_price)):
+        for j in range(len(transport_table_price[0])):
+            if max < transport_table_price[i][j]:
+                max = transport_table_price[i][j]
+                max_line = i
+                max_colomn = j
+
+    return max_line, max_colomn
+
+
+def potential_method(transport_table_price, transportation_burden, demand_colomn, production_line):
+    line_potential = []
+    colomn_potencial = []
